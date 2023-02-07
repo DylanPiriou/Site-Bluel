dd18078551ee1fe828d5162961d3f229a67eb31d
DylanPiriou <dylan.piriou@hotmail.com>
2023-02-07 13:59:18 +0100

    getworks function improved

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -25,7 +25,7 @@ window.onload = () => {
     });
   });
 
-  // Fonction de filtrage
+  // Logique de filtrage
   function filterByCategory(category) {
     const filter = new Map();
     filter.set("category1", "");
@@ -35,29 +35,38 @@ window.onload = () => {
     getWorks(category, filter)
   }
 
+  // Fonction qui recupère les données soit de l'API soit du tableau
   function getWorks(category, filter) {
-    // Si array vide alors fetch sino utiliser array
-    !!arrayData &&
+    if (arrayData.length === 0) {
       fetch("http://localhost:5678/api/works")
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
           // Les données reçues de l'API sont mises dans le tableau arrayData
           arrayData = data;
-          // mettre dans une fonction séparée
-          let filteredData;
-          if (category === "category1") {
-            filteredData = arrayData;
-          } else {
-            const value = filter.get(category);
-            filteredData = arrayData.filter((work) => work.category.name === value);
-          }
-          handleModal();
-          handleGallery(filteredData)
+          filterAllData(category, filter)
         }).catch(err => console.log(err))
+    } else {
+      filterAllData(category, filter)
+    }
   }
 
-  // Fonction qui permet de créer les éléments
+  // Fonction qui permet de filtrer la data
+  function filterAllData(category, filter) {
+    let filteredData;
+    if (category === "category1") {
+      // On affiche tout
+      filteredData = arrayData;
+    } else {
+      // On filtre l'affichage en fonction du nom de la catégorie
+      const value = filter.get(category);
+      filteredData = arrayData.filter((work) => work.category.name === value);
+    }
+    handleModal();
+    handleGallery(filteredData)
+  }
+
+  // Fonction qui permet de créer les éléments dans la gallerie
   function addImgsToGallery(item) {
     const figure = document.createElement("figure");
     const img = document.createElement("img");
@@ -71,13 +80,6 @@ window.onload = () => {
     return figure;
   }
 
-  // Fonction qui permet d'ajouter les éléments dans la balise "gallery" via une boucle
-  function addToGallery(element) {
-    gallery &&
-      gallery.appendChild(element)
-
-  }
-
   // Parcours les éléments filtrés, crée les éléments et les ajoute à la "gallery"
   function handleGallery(filteredData) {
     filteredData.forEach((item) => {
@@ -85,5 +87,12 @@ window.onload = () => {
       addToGallery(element);
     })
   }
+  
+  // Fonction qui permet d'ajouter les éléments dans la balise "gallery" via une boucle
+  function addToGallery(element) {
+    gallery &&
+      gallery.appendChild(element)
+  }
+
 
 }
